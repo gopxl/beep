6e88eec2c9c8ebf7857a9b059693f69a873fd77f
Mark Kremer <mail@markkremer.nl>
2023-11-02 18:53:06 +0100

    Benchmark the speaker conversion from samples to bytes

---
+++ speaker/speaker_test.go
@@ -0,0 +1,36 @@
+package speaker
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/gopxl/beep/internal/testtools"
+)
+
+func BenchmarkSampleReader_Read(b *testing.B) {
+	// note: must be multiples of bytesPerSample
+	bufferSizes := []int{64, 512, 8192, 32768}
+
+	for _, bs := range bufferSizes {
+		b.Run(fmt.Sprintf("with buffer size %d", bs), func(b *testing.B) {
+			s, _ := testtools.RandomDataStreamer(b.N)
+			r := newReaderFromStreamer(s)
+			buf := make([]byte, bs)
+
+			b.StartTimer()
+			for {
+				n, err := r.Read(buf)
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					panic(err)
+				}
+				if n != len(buf) {
+					break
+				}
+			}
+		})
+	}
+}
