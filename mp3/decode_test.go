84dd3d811a2ce7fe0d5f2d334b6ca2fba16c35ee
Mark Kremer <mail@markkremer.nl>
2023-11-04 17:50:00 +0100

    Test Streamer return values of different audio format decoders

--- mp3/decode_test.go
+++ mp3/decode_test.go
@@ -1,4 +1,4 @@
-package flac_test
+package mp3_test
 
 import (
 	"os"
@@ -6,18 +6,18 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/gopxl/beep/flac"
 	"github.com/gopxl/beep/internal/testtools"
+	"github.com/gopxl/beep/mp3"
 )
 
 func TestDecoder_ReturnBehaviour(t *testing.T) {
-	f, err := os.Open(testtools.TestFilePath("valid_44100hz_22050_samples.flac"))
+	f, err := os.Open(testtools.TestFilePath("valid_44100hz_22050_samples.mp3"))
 	assert.NoError(t, err)
 	defer f.Close()
 
-	s, _, err := flac.Decode(f)
+	s, _, err := mp3.Decode(f)
 	assert.NoError(t, err)
-	assert.Equal(t, 22050, s.Len())
+	//assert.Equal(t, 22050, s.Len()) // todo: mp3 seems to return more samples than there are in the file. Uncomment this when fixed.
 
 	testtools.AssertStreamerHasCorrectReturnBehaviour(t, s, s.Len())
 }
