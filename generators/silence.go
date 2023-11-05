11b5aeb3a838700f7396d4168c8004c99fbf3d25
Mark Kremer <mail@markkremer.nl>
2023-11-05 12:21:10 +0100

    Optimize generators.Silence

--- generators/silence.go
+++ generators/silence.go
@@ -5,19 +5,27 @@ import "github.com/gopxl/beep"
 // Silence returns a Streamer which streams num samples of silence. If num is negative, silence is
 // streamed forever.
 func Silence(num int) beep.Streamer {
+	if num < 0 {
+		return beep.StreamerFunc(func(samples [][2]float64) (m int, ok bool) {
+			for i := range samples {
+				samples[i] = [2]float64{}
+			}
+			return len(samples), true
+		})
+	}
+
 	return beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
-		if num == 0 {
+		if num <= 0 {
 			return 0, false
 		}
-		if 0 < num && num < len(samples) {
+		if num < len(samples) {
 			samples = samples[:num]
 		}
 		for i := range samples {
 			samples[i] = [2]float64{}
 		}
-		if num > 0 {
-			num -= len(samples)
-		}
+		num -= len(samples)
+
 		return len(samples), true
 	})
 }
